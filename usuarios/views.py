017fcdb37a0eb6fb1060f14224fb2a08f192e246
Jorge052011 <jimunozacuna+1@gmail.com>
2025-08-01 02:58:36 +0000

    W

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms  import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView,TemplateView, UpdateView
+from .form import FormularioRegistroUsuario
+from django.urls import reverse_lazy
 
 # Create your views here.
-
+"""
 def registro(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
@@ -12,3 +17,17 @@ def registro(request):
     else:
         form = UserCreationForm()        
     return render(request, "usuarios/registro.html",{"form": form})
+"""
+class RegistroUsuario(SuccessMessageMixin, CreateView):
+    form_class = FormularioRegistroUsuario
+    template_name = "usuarios/registro.html"
+    success_url = reverse_lazy('login')
+    success_message = 'usuario registrado correctamente'
+
+
+class LoginUsuario(LoginView):
+    template_name = "usuarios/login.html"
+
+class LogoutUsuario(LoginRequiredMixin, LogoutView):
+    next_page = reverse_lazy('home')
+    http_method_names = ['get', 'post']
